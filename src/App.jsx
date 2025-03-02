4bb259af45eaa7d0fbfc8d4b353251b3d38a8310
oguz <ogzhnyilmzz@outlook.com>
2025-03-02 21:55:03 +0300

    Hatalar giderildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import backgroundImage from "../public/images/bg-image.jpg";
 
 export default function TodoApp() {
   const [todos, setTodos] = useState([]);
@@ -45,7 +46,7 @@ export default function TodoApp() {
   });
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat px-4 md:px-10 lg:px-20" style={{ backgroundImage: "url('/images/bg-image.jpg')" }}>
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat px-4 md:px-10 lg:px-20" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="w-full max-w-md md:max-w-lg lg:max-w-2xl p-4 bg-gray-100 bg-opacity-90 rounded-lg shadow-lg mt-10">
         <h1 className="text-2xl font-bold text-center mb-4">Todo React</h1>
         <div className="flex flex-col sm:flex-row mb-4">
